d706cc9d8fbabb096e89d22b120d2f005e44f0c3
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2024-12-14 17:28:33 +0000

    Created project for in-memory repositories that read application data from the JSON files into memory.
    Also created test project for the above project

---
+++ Formula1Standings.InMemoryRepositories/InMemoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Formula1Standings.Models;
+using System.Text.Json;
+
+namespace Formula1Standings.InMemoryRepositories
+{
+    public class InMemoryRepository<T>
+    {
+        private readonly List<T> _records = new List<T>();
+
+        public IList<T> GetAll()
+        {
+            return _records.ToArray();
+        }
+
+        public async Task LoadFromJsonFile(string path)
+        {
+            using var jsonFile = File.OpenRead(path);
+            var allRecords = await JsonSerializer.DeserializeAsync<T[]>(jsonFile);
+            _records.AddRange(allRecords!);
+        }
+    }
+}
